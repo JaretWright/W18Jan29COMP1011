c386399ffcd7e2554b4d7a6753e7f10513284604
JaretWright <hkkA223>
2018-02-05 10:25:29 -0500

    Added slider

--- src/w18comp1011jan29/PhoneViewController.java
+++ src/w18comp1011jan29/PhoneViewController.java
@@ -5,6 +5,8 @@ import java.util.ResourceBundle;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.Slider;
 import javafx.scene.control.TextField;
 
 
@@ -19,7 +21,8 @@ public class PhoneViewController implements Initializable {
     @FXML    private ComboBox<String> brandComboBox;
     @FXML    private TextField modelTextField;
     @FXML    private TextField osTextField;
-
+    @FXML    private Slider resolutionSlider;
+    @FXML    private Label resolutionLabel;
     
     public void createPhoneButtonPushed()
     {
@@ -38,9 +41,25 @@ public class PhoneViewController implements Initializable {
     @Override
     public void initialize(URL location, ResourceBundle resources)
     {
+        //setup the combobox
         brandComboBox.getItems().addAll(Phone.getManufacturers());
         brandComboBox.getSelectionModel().selectFirst();
         
+        resolutionSlider.setMin(4);
+        resolutionSlider.setMax(20);
+        resolutionSlider.setValue(12); //set the default value
+        resolutionLabel.setText(Double.toString(resolutionSlider.getValue())
+                                                +" MP");
+    }
+    
+    /**
+     * This method will update the resolution label and should be called
+     * when the slider is dragged
+     */
+    public void resolutionSliderMoved()
+    {
+        String label = String.format("%.1f MP", resolutionSlider.getValue());
+        resolutionLabel.setText(label);
     }
     
 }
